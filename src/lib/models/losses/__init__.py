8f5dc81a600ba1618952c2acc38387306397c663
aihacker111 <tinprocoder0908@gmail.com>
2026-05-17 15:12:43 +0700

    udpate cfg

--- src/lib/models/losses/__init__.py
+++ src/lib/models/losses/__init__.py
@@ -1,7 +1,7 @@
 from .matcher import HungarianMatcher, box_cxcywh_to_xyxy, generalized_box_iou
-from .hybrid_loss import HybridLoss, sigmoid_focal_loss, centernet_focal_loss
+from .hybrid_loss import HybridLoss, centernet_focal_loss
 
 __all__ = [
     'HungarianMatcher', 'box_cxcywh_to_xyxy', 'generalized_box_iou',
-    'HybridLoss', 'sigmoid_focal_loss', 'centernet_focal_loss',
+    'HybridLoss', 'centernet_focal_loss',
 ]
